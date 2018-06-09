23df845c9e5a7556bbcd2f9121efe1f0743a9327
Elizabith Costey <elizabithcostey@Elizabiths-MacBook-Air.local>
2018-06-09 00:36:06 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
-	} else {alert("Sorry, try again.")}
+
+var checkForMatch = function() {
+    if (cardsInPlay.length >= 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!")
+        } else {
+            alert("Sorry, try again.");
+        }
+    }
 }
+var flipcard = function(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped" + " " + cards[cardId]);
+    checkForMatch();
+}
+flipcard(0);
+flipcard(2);
